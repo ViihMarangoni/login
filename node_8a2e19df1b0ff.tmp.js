3322c8b499198c758485f2b05fe3124e56ff629b
ViihMarangoni <142364559+ViihMarangoni@users.noreply.github.com>
2024-02-07 22:07:32 -0300

    login

---
+++ node_8a2e19df1b0ff.tmp.js
@@ -0,0 +1,29 @@
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: "poppins", sans-serif;
+}
+
+body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    background-color: aquamarine;
+}
+
+.login {
+    width: 420px;
+    background-color: purple;
+    color: white;
+}
+
+.login h1 {
+    font-size: 36px;
+    text-align: center;
+}
+
+.login caixa-entrada {
+    wid
+}
